08894d18badc1982cbcbfb702d85825414ddb78f
f1am3d <flamesoff@gmail.com>
2026-09-17 10:10:26 +0200

    rt: put the Q2RTX licence back on the renderer files ported from it
    26 renderer files are ports of Quake 2 RTX sources but carried Sultim
    Tsyrendashiev's MIT header. None of them exists in upstream RayTracedGL1
    (all branches and tags were checked: the Q2 layer is ours, not his), so
    the MIT notice never applied to them.
    
    Each file now carries the GPL v2 block of the Q2RTX file it is a port of,
    with that file's own copyright holders, and names that file:
    
      CmGodRays* <- god_rays*, CmProceduralSky <- physical_sky,
      CmQ2Adapter <- compositing, CmQ2Atrous* <- asvgf_atrous/asvgf_lf,
      CmQ2Gradient* <- asvgf_gradient_*, CmQ2Interleave <-
      checkerboard_interleave, CmQ2TAAU <- asvgf_taau, CmQ2Temporal <-
      asvgf_temporal, Q2Asvgf.h <- asvgf.glsl, Q2Fog.h/CmQ2Fog <- fog.h,
      Q2LightLists.h <- light_lists.h, RtQ2Indirect <- indirect_lighting,
      RtQ2ReflectRefract <- reflect_refract, ShadowMap.vert <- shadow_map.vert,
      TonemappingUtils.glsl <- tone_mapping_utils.glsl,
      GodRays/ShadowMap <- god_rays.c/shadow_map.c, Q2Denoiser <- asvgf.c.
    
    TurbWarp.h, RayStats.* and PassTimings.* are our own code but had a
    copyright line without a holder; they now name the project. The stray
    Build_test.spv is removed. Comments only - no line of code changed.

--- vkpt/Source/Q2Denoiser.cpp
+++ vkpt/Source/Q2Denoiser.cpp
@@ -1,22 +1,25 @@
-// Copyright (c) 2020-2021 Sultim Tsyrendashiev
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// Copyright (C) 2018 Christoph Schied
+// Copyright (C) 2019, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2026 QuakeRay contributors
+//
+// This file is a port of asvgf.c from Quake 2 RTX (https://github.com/NVIDIA/Q2RTX),
+// which is distributed under the terms of the GNU General Public License
+// version 2.  It has been adapted to the renderer interface of this project.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 
 #include "Q2Denoiser.h"
 
--- vkpt/Source/Shaders/TurbWarp.h
+++ vkpt/Source/Shaders/TurbWarp.h
@@ -1,3 +1,19 @@
+// Copyright (c) 2026 QuakeRay contributors
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 // The classic "warp" of the turbulent surfaces (lava, teleport): every texture
 // axis is shifted by a sine of the other axis, so the pattern swirls in time.
 //
@@ -8,6 +24,7 @@
 // the period is therefore two texture tiles and the amplitude an eighth of a
 // tile. The phase grows by one radian per second, which is also the unit of
 // globalUniform.time.
+//
 
 vec2 getTurbWarpUV(const vec2 texCoord)
 {
